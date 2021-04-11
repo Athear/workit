7345ebfa0e45450b6e6b0c4f63da23573b2b80c5
Todd Malone <toddomalone@gmail.com>
2021-04-10 22:32:16 -0500

    added scaffolding for home routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+//route for index (for completeness)
+router.get('/',(req,res)=>{
+    
+});
+
+
+//route for exercise
+//new workout
+router.get('/exercise',(req,res)=>{
+    
+});
+
+//route for exercise
+//continue workout. May not need
+router.get('/exercise?',(req,res)=>{
+    
+});
+
+//route for stats
+router.get('/stats',(req,res)=>{
+    
+})
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const workout = require('./workoutRoutes');
+const home = require('./homeRoutes')
 
+router.use('/',home);
 router.use('/api/workout', workout);
 
 module.exports = router;
