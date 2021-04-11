89ad15d77235f067b833f39894b8ae4a620f42ea
Todd Malone <toddomalone@gmail.com>
2021-04-10 21:36:57 -0500

    scaffolding for server-side routes

--- controllers/workoutRoutes.js
+++ controllers/workoutRoutes.js
@@ -1,2 +1,26 @@
 const router = require('express').Router();
 const db = require("../models");
+
+//route for getLastWorkout
+router.get('/',(req,res)=>{
+    
+});
+
+//route for addExercise
+router.put('/',(req,res)=>{
+    db.Exercise.create(req.body)
+        .then({_id}) => db.Exercise.findOneAndUpdate({},//TODO: filter properly!
+                {$push:{ exercises: _id }},
+                {new:true}
+            )
+});
+
+//route for createWorkout
+router.post('/',(req,res)=>{
+    
+});
+
+//route for getWorkoutsInRange
+router.get('/range',(req,res)=>{
+    
+});
