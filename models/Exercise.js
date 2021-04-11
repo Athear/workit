ba4014f49aca67fe26b200a6234ad46bf4646901
Todd Malone <toddomalone@gmail.com>
2021-04-10 20:47:33 -0500

    fleshed out Exercise model

--- models/Exercise.js
+++ models/Exercise.js
@@ -7,7 +7,30 @@ const ExerciseSchema = new Schema({
     type: String,
     required:true
   },
- 
+  name: {
+    type: String,
+    required:true
+  },
+  duration: {
+    type: Number,
+    required:true
+  },
+  distance: {
+    type: Number,
+    required:false,
+  },
+  weight: {
+    type: Number,
+    required:false,
+  },
+  reps: {
+    type: Number,
+    required:false,
+  },
+  sets: {
+    type: Number,
+    required:false,
+  },
 });
 
 const Exercise = mongoose.model("Exercise", ExerciseSchema);
