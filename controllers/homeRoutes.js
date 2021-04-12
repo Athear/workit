772225bc0b81d9f611bae262eed053df58d2ed69
Todd Malone <toddomalone@gmail.com>
2021-04-11 20:49:24 -0500

    html routes are accessible

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,26 +1,27 @@
 const router = require('express').Router();
+var path = require("path");
 
 //route for index (for completeness)
 router.get('/',(req,res)=>{
-    
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 
 //route for exercise
 //new workout
 router.get('/exercise',(req,res)=>{
-    
+    res.sendFile(path.join(__dirname, "../public/exercise.html")); 
 });
 
 //route for exercise
 //continue workout. May not need
 router.get('/exercise?',(req,res)=>{
-    
+    res.sendFile(path.join(__dirname, "../public/exercise.html")); 
 });
 
 //route for stats
 router.get('/stats',(req,res)=>{
-    
+    res.sendFile(path.join(__dirname, "../public/stats.html")); 
 })
 
 module.exports = router;
